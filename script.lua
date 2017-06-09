0e9c9e823e4225ccd8a63374035800a6209de693
Liam Byrne <liam.jb1@gmail.com>
2017-06-09 15:43:39 +1200

    Create script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,12 @@
+-- 9 June 2017
+-- Marble Turtle Tekkit Script
+-- @author Liam Byrne            github.com/byrneliam2 
+-- @author Andrew McManaway      github.com/McManaway1
+
 function checkSlots()
+    --[[
+    Check that the inventory of the turtle is full or not.
+    ]]--
     full = true
     for i = 1, 9 do
         if (turtle.getItemCount(i) ~= 64) then
