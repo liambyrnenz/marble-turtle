0464295173da95a5b049996d41fecb0feb44c4df
Liam Byrne <liam.jb1@gmail.com>
2017-06-09 15:59:25 +1200

    Update

--- script.lua
+++ script.lua
@@ -3,6 +3,11 @@
 -- @author Liam Byrne            github.com/byrneliam2 
 -- @author Andrew McManaway      github.com/McManaway1
 
+-- ================== References =====================
+-- https://www.lua.org/manual/5.1/manual.html
+-- http://computercraft.info/wiki/Turtle_(API)
+-- http://www.computercraft.info/wiki/Os.loadAPI
+
 function checkSlots()
     --[[
     Check that the inventory of the turtle is full or not.
